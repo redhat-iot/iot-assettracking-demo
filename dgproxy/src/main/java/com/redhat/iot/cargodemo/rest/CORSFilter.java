49fcb127b5683412ad6d34fed68f2a944328bffd
jamesfalkner <schtool@gmail.com>
2017-05-22 18:07:37 -0400

    Move to Eclipse Public License 1.0

--- dgproxy/src/main/java/com/redhat/iot/cargodemo/rest/CORSFilter.java
+++ dgproxy/src/main/java/com/redhat/iot/cargodemo/rest/CORSFilter.java
@@ -1,19 +1,18 @@
-package com.redhat.iot.cargodemo.rest;/*
- * JBoss, Home of Professional Open Source
- * Copyright 2015, Red Hat, Inc. and/or its affiliates, and individual
- * contributors by the @authors tag. See the copyright.txt in the
- * distribution for a full listing of individual contributors.
+/*
+ * ******************************************************************************
+ * Copyright (c) 2017 Red Hat, Inc. and others
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat Inc. - initial API and implementation
+ *
+ * ******************************************************************************
  */
+package com.redhat.iot.cargodemo.rest;
 
 import java.io.IOException;
 
--- openshift-common.sh
+++ openshift-common.sh
@@ -1,14 +1,16 @@
-###############################################################################
-# Copyright (c) 2016, 2017 Red Hat Inc and others
+#!/usr/bin/env bash
+#*******************************************************************************
+# Copyright (c) 2017 Red Hat, Inc and others
 #
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License v1.0
 # which accompanies this distribution, and is available at
 # http://www.eclipse.org/legal/epl-v10.html
 #
-###############################################################################
-
-#!/usr/bin/env bash
+# Contributors:
+#     Red Hat, Inc. - initial API and implementation
+#
+#******************************************************************************
 
 export OPENSHIFT_PROJECT_NAME=${OPENSHIFT_PROJECT_NAME:=redhat-iot}
 
